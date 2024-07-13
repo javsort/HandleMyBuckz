87912514d6d2b77a58b6842f110b1eac8cd10f36
Javier Ortega Mendoza <javierortega336@gmail.com>
2024-07-13 16:28:15 +0200

    Initial commit

---
+++ src/main/java/javsort/handlemybuckz/HandleMyBuckzApplication.java
@@ -0,0 +1,13 @@
+package javsort.handlemybuckz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleMyBuckzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleMyBuckzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javsort/handlemybuckz/HandleMyBuckzApplicationTests.java
@@ -0,0 +1,13 @@
+package javsort.handlemybuckz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleMyBuckzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
